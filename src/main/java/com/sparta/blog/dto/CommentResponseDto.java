4bc7a59485a57c0fb78ffdf903f89e3b8e955196
jehyun-park <wpus6242@cu.ac.kr>
2024-02-09 00:16:35 +0900

    feat:게시물 조회(수정)

--- src/main/java/com/sparta/blog/dto/CommentResponseDto.java
+++ src/main/java/com/sparta/blog/dto/CommentResponseDto.java
@@ -9,11 +9,9 @@ import com.sparta.blog.entity.Comment;
 @Getter
 @NoArgsConstructor
 public class CommentResponseDto {
-    private Long id;
     private String content;
     private String username;
     public CommentResponseDto(Comment comment) {
-        this.id = comment.getId();
         this.content = comment.getContent();
         this.username = comment.getUser().getUsername();
     }
--- src/main/java/com/sparta/blog/service/BoardService.java
+++ src/main/java/com/sparta/blog/service/BoardService.java
@@ -8,6 +8,7 @@ import com.sparta.blog.repository.CommentRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
+import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -20,35 +21,23 @@ public class BoardService {
 
 
     // 게시물 전체 목록 조회
-    @Transactional
+    @Transactional(readOnly = true)
     public List<BoardResponseDto> getBoardList() {
-        List<Board> boards = boardRepository.findAllByOrderByCreatedAtDesc();
-
-        List<BoardResponseDto> boardResponseDtos = boards.stream()
-                .map(board -> {
-                    List<CommentResponseDto> commentList = board.getCommentList()
-                            .stream()
-                            .map(CommentResponseDto::new)
-                            .collect(Collectors.toList());
-                    return new BoardResponseDto(board, commentList);
-                })
+        return boardRepository.findAllByOrderByCreatedAtDesc().stream()
+                .map(board -> new BoardResponseDto(board, new ArrayList<>()))
                 .collect(Collectors.toList());
-
-        return boardResponseDtos;
     }
 
-
     // 특정 게시물 조회
-    @Transactional
+    @Transactional(readOnly = true)
     public BoardResponseDto getBoardDetail(Long id) {
         Board board = boardRepository.findById(id).orElseThrow(
                 () -> new IllegalArgumentException("글이 존재하지 않습니다.")
         );
-        List<CommentResponseDto> commentList = commentRepository.findById(id)
+        List<CommentResponseDto> commentList = commentRepository.findAllByBoardsId(id)
                 .stream()
                 .map(CommentResponseDto::new)
                 .collect(Collectors.toList());
-        return new BoardResponseDto(board,commentList);
+        return new BoardResponseDto(board, commentList);
     }
-
 }
