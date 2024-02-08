0ebba787b19e740e92213c0d08674c2fa65db987
Esunsin <jinchan1148@gmail.com>
2024-02-08 22:24:30 +0900

    style

--- src/main/java/com/sparta/blog/dto/CommonResponseDto.java
+++ src/main/java/com/sparta/blog/dto/CommonResponseDto.java
@@ -12,7 +12,6 @@ import lombok.Setter;
 @AllArgsConstructor
 @JsonInclude(JsonInclude.Include.NON_NULL)
 public class CommonResponseDto {
-    private String msg;
-    private Integer statusCode;
-
+	private String msg;
+	private Integer statusCode;
 }
