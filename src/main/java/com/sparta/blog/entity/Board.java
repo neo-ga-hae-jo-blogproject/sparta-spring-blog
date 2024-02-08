7199ff9770a209faeab4d2c631e1c150b3fc3ad2
oyeonjoo <oyj0842@naver.com>
2024-02-08 10:36:03 +0900

    git init

--- src/main/java/com/sparta/blog/entity/Board.java
+++ src/main/java/com/sparta/blog/entity/Board.java
@@ -1,4 +1,25 @@
 package com.sparta.blog.entity;
 
-public class Board {
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "boards")
+public class Board extends Timestamped {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "title", nullable = false)
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+//    @ManyToOne
+//    @JoinColumn(name = "user_id")
+//    private User user;
 }
