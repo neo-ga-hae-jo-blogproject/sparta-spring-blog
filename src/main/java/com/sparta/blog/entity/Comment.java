c44103ff0ed9482e8ac798ef13d41e71ed377b92
Esunsin <jinchan1148@gmail.com>
2024-02-08 10:56:18 +0900

    add : comment entity

--- src/main/java/com/sparta/blog/entity/Comment.java
+++ src/main/java/com/sparta/blog/entity/Comment.java
@@ -1,4 +1,20 @@
 package com.sparta.blog.entity;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@Table(name = "commnets")
 public class Comment {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String content;
+
+
 }
