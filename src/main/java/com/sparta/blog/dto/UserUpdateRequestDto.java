1a1136bc020342b3b536d8d08b2026cb67b117d9
oyeonjoo <oyj0842@naver.com>
2024-02-14 04:18:59 +0900

    refactor: @NotBlank 어노테이션 추가

--- src/main/java/com/sparta/blog/dto/UserUpdateRequestDto.java
+++ src/main/java/com/sparta/blog/dto/UserUpdateRequestDto.java
@@ -1,5 +1,6 @@
 package com.sparta.blog.dto;
 
+import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.Getter;
 import lombok.Setter;
@@ -7,11 +8,13 @@ import lombok.Setter;
 @Getter
 @Setter
 public class UserUpdateRequestDto {
-
+    @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
     private String oldPassword;
 
+    @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
     @Pattern(regexp = "^[a-zA-Z0-9]{8,15}$")
     private String newPassword;
 
+    @NotBlank(message = "비밀번호는 필수로 입력해야 합니다.")
     private String newPasswordCheck;
 }
