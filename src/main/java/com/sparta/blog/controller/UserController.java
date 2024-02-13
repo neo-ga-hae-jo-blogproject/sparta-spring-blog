5379b22f793c6323a88b00814a02d1878a4f780a
Esunsin <jinchan1148@gmail.com>
2024-02-13 12:59:38 +0900

    test

--- src/main/java/com/sparta/blog/controller/UserController.java
+++ src/main/java/com/sparta/blog/controller/UserController.java
@@ -8,7 +8,9 @@ import com.sparta.blog.entity.User;
 import com.sparta.blog.jwt.JwtUtil;
 import com.sparta.blog.service.UserService;
 import jakarta.persistence.Id;
+import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.http.HttpSession;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -41,15 +43,36 @@ public class UserController {
 
 
     @PostMapping("/signin")
-    public ResponseEntity<CommonResponseDto> login(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response) {
+    public ResponseEntity<CommonResponseDto> login(@RequestBody UserRequestDto userRequestDto, HttpServletResponse response, HttpServletRequest request) {
         try {
+            // 이미 로그인된 사용자인지 확인
+            if (userService.isUserLoggedIn(userRequestDto.getEmail(), request)) {
+                return ResponseEntity.badRequest().body(new CommonResponseDto("이미 로그인된 사용자입니다.", HttpStatus.BAD_REQUEST.value()));
+            }
+
             userService.login(userRequestDto);
         } catch (IllegalArgumentException e) {
             return ResponseEntity.badRequest().body(new CommonResponseDto(e.getMessage(), HttpStatus.BAD_REQUEST.value()));
         }
+
         response.setHeader(JwtUtil.AUTHORIZATION_HEADER, jwtUtil.createToken(userRequestDto.getEmail()));
+
         return ResponseEntity.ok().body(new CommonResponseDto("로그인 성공", HttpStatus.OK.value()));
     }
+
+    @PostMapping("/signout")
+    public ResponseEntity<CommonResponseDto> logout(HttpServletRequest request) {
+        HttpSession session = request.getSession(false);
+
+        if (session != null && session.getAttribute("loggedInUser") != null) {
+            // 세션에서 로그인 정보를 제거
+            session.removeAttribute("loggedInUser");
+            return ResponseEntity.ok(new CommonResponseDto("로그아웃이 완료되었습니다.", HttpStatus.OK.value()));
+        } else {
+            return ResponseEntity.badRequest().body(new CommonResponseDto("로그인 상태가 아닙니다.", HttpStatus.BAD_REQUEST.value()));
+        }
+    }
+
     //조회
     @GetMapping("/{userId}")
     public UserResponseDto getUserList(@PathVariable Long userId){
