eaf8b10fed4d2b8b34da6acf724c9a607866b8a0
oyeonjoo <oyj0842@naver.com>
2024-02-14 04:19:53 +0900

    feat: Exception 추가

---
+++ src/main/java/com/sparta/blog/exception/dto/ExceptionDto.java
@@ -0,0 +1,19 @@
+package com.sparta.blog.exception.dto;
+
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@Setter
+@Builder
+public class ExceptionDto {
+
+    private int statusCode;
+    private HttpStatus state;
+    private String message;
+
+}
+
